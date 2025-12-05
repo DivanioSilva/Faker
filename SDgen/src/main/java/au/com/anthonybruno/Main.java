04f3da0b87a14da7f8e64d6740552ab419321c26
Divanio Silva <divanios@icloud.com>
2025-12-05 15:18:01 +0000

    Creating a multi module project with java-faker and SDgen projects

---
+++ SDgen/src/main/java/au/com/anthonybruno/Main.java
@@ -0,0 +1,8 @@
+package au.com.anthonybruno;
+
+public class Main {
+    public static void main(String[] args) {
+//        Gen.start()
+//                .use()
+    }
+}
---
+++ java-faker/src/main/java/com/github/javafaker/app/Main.java
@@ -0,0 +1,19 @@
+package com.github.javafaker.app;
+
+import au.com.anthonybruno.Gen;
+import com.github.javafaker.Faker;
+
+public class Main {
+    public static void main(String[] args) {
+        Faker faker = Faker.instance();
+        Gen.start()
+                .addField("username",() -> faker.name().username())
+                .addField("firstName",() -> faker.name().firstName())
+                .addField("lastName",() -> faker.name().lastName())
+                .addField("Age", () -> faker.number().numberBetween(18, 30))
+                .addField("email", () -> faker.internet().emailAddress())
+                .generate(1000)
+                .asCsv()
+                .toFile("teste.csv");
+    }
+}
